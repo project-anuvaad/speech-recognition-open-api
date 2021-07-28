1fc1948cee879f6c1e7e8a92e8d807602ece48ac
aswinpradeep <aswinpradeep1@gmail.com>
2021-07-28 15:50:20 +0530

    remove unnecessary dir change

--- srt/subtitle_generator.py
+++ srt/subtitle_generator.py
@@ -27,10 +27,9 @@ def media_conversion(file_name, duration_limit=5):
     return dir_name
 
 def noise_suppression(dir_name,denoiser_path):
-    cwd = os.getcwd()
-    os.chdir(denoiser_path)
+
     subprocess.call(["python -m denoiser.enhance --dns48 --noisy_dir {} --out_dir {} --sample_rate {} --num_workers {} --device cpu".format(dir_name, dir_name, 16000, 1)], shell=True)
-    os.chdir(cwd)
+ 
 
 def get_srt(file_name, model, generator, dict_path, denoiser_path, audio_threshold=5, language='hi', half = False):
     dir_name = media_conversion(file_name, duration_limit=audio_threshold)

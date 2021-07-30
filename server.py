adafb42f55baad0936a3babb8515ba68effe2d20
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2021-07-30 20:18:39 +0530

    added max size file 50MB  to server.py

--- server.py
+++ server.py
@@ -6,10 +6,16 @@ from lib.inference_lib import Wav2VecCtc
 from auth_interceptor import AuthInterceptor
 
 
+MAX_MESSAGE_LENGTH = 50 * 1024 * 1024
+
 def run():
     server = grpc.server(
         futures.ThreadPoolExecutor(max_workers=10),
         # interceptors=(AuthInterceptor('Bearer mysecrettoken'),)
+        
+    	options = [('grpc.max_send_message_length', MAX_MESSAGE_LENGTH),('grpc.max_receive_message_length', MAX_MESSAGE_LENGTH)] 
+        
+        
     )
     add_SpeechRecognizerServicer_to_server(SpeechRecognizer(), server)
     server.add_insecure_port('[::]:50051')
